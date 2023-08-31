53a86b3b1c14bd7de234f58104ba8897446d948e
Paulo Luan <pauloluan.inova@gmail.com>
2023-08-31 08:41:43 -0300

    fixed tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Form } from "devlink/Form";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello Frontinvale
+    <main className="bg-background min-h-screen w-full flex items-center justify-center">
+      <div className="max-w-[600px] h-full">
+        <h1 className="text-6xl font-semibold">Hello Frontinvale</h1>
+        <p className="text-lg">
+          this is a component that was fetched from webflow:
+        </p>
+        <Form />
+      </div>
     </main>
   );
 }
